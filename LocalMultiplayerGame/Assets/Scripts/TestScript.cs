7829faf6b08294ed6fbb921ebf172866bfca3486
PickleDan74 <153804889+PickleDan74@users.noreply.github.com>
2023-12-18 19:58:37 +1030

    Scripts setup
    Put a test script and a script folder in the project.

---
+++ LocalMultiplayerGame/Assets/Scripts/TestScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour{
+
+    //This is a test script 
+    void Start(){
+        
+    }
+    void Update(){
+        
+    }
+}
